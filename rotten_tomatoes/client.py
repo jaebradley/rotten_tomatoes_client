7ff4916731d5a76270a5a2a0098408ef7db3ad81
jaebradley <jae.b.bradley@gmail.com>
2017-05-26 00:54:17 -0400

    initial

---
+++ rotten_tomatoes/client.py
@@ -0,0 +1,39 @@
+import requests
+
+from rotten_tomatoes.query.parameters.builders.browsing import MovieBrowsingQueryParametersBuilder
+from rotten_tomatoes.query.parameters.browsing import TvBrowsingCategory
+
+
+class RottenTomatoesClient:
+    BASE_URL = "https://www.rottentomatoes.com/api/private/v2.0"
+    SEARCH_URL = "{base_url}/search".format(base_url=BASE_URL)
+    BROWSE_URL = "{base_url}/browse".format(base_url=BASE_URL)
+
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def search(term):
+        r = requests.get(url=RottenTomatoesClient.SEARCH_URL, params={"q": term})
+
+        r.raise_for_status()
+
+        return r.json()
+
+    @staticmethod
+    def browse_movies(query):
+        parameters = MovieBrowsingQueryParametersBuilder.build(query=query)
+
+        r = requests.get(url=RottenTomatoesClient.BROWSE_URL, params=parameters)
+
+        r.raise_for_status()
+
+        return r.json()
+
+    @staticmethod
+    def browse_tv_shows(category=TvBrowsingCategory.most_popular):
+        r = requests.get(url=RottenTomatoesClient.BROWSE_URL, params={"type": category.value})
+
+        r.raise_for_status()
+
+        return r.json()
